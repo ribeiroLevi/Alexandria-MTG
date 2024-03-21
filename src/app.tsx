ea4f07c3256ede89ba65d0ba436573eb61fe65bc
Levi Ribeiro <leviribeiro72@gmail.com>
2024-03-21 13:38:43 -0300

    ":sparkles: feat: router and multiple pages"

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,15 @@
-import { Search } from 'lucide-react';
+import { Landing } from './components/landing';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Lib } from './components/lib';
 
 export function App() {
   return (
-    <div className="overflow-hidden flex items-center flex-col">
-      <nav className="py-5 flex flex-row w-full">
-        <div className="w-full flex flex-row items-center justify-between">
-          <h1 className="mx-auto font-Karantina text-orange-900 text-5xl flex">
-            ALEXANDRIA
-          </h1>
-        </div>
-      </nav>
-      <div className="-mt-[88px] h-screen flex items-center justify-center flex-col">
-        <h1 className="text-[150px] font-Lusitana text-orange-900">
-          Alexandria
-        </h1>
-        <p className="font-Karantina text-[40px] -mt-16 text-orange-700">
-          A Verdadeira Biblioteca de Magic
-        </p>
-        <button className="bg-orange-900 w-[250px] h-[40px] rounded-md font-Karatina font-bold text-orange-200 text-xl pb-1">
-          Explorar
-        </button>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/lib" element={<Lib />} />
+      </Routes>
+    </Router>
   );
 }
