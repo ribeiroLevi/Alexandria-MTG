bc1915710d7ed2703969fef30ee9f30ad5ef7af6
Levi Ribeiro <leviribeiro72@gmail.com>
2024-03-21 21:02:58 -0300

    :sparkles: feat: cards query with input

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,55 +1,113 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import axios from 'axios';
 
 export function Lib() {
   const [cards, setCards] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [randomPlaceholder, setRandomPlaceholder] = useState('');
   const pageSize = 100;
 
   useEffect(() => {
-    async function  fetchData() {
-      try{
-        const response = await axios.get (`https://api.magicthegathering.io/v1/cards?page=${currentPage}&pageSize=${pageSize}`)
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `https://api.magicthegathering.io/v1/cards?page=${currentPage}&pageSize=${pageSize}`
+        );
         const filteredCards = filterUniqueNames(response.data.cards);
-        setCards(prevCards => [...prevCards, ...filteredCards]);
-      }catch(error){
-        console.log ("Error", error);
+        setCards((prevCards) => [...prevCards, ...filteredCards]);
+      } catch (error) {
+        console.log('Error', error);
       }
     }
-    fetchData()
-  }, [currentPage])
+    fetchData();
+  }, [currentPage]);
 
-  //filtro de cartas repetidas
-  const filterUniqueNames = (cards) =>{
+  // Filter out cards with duplicate names
+  const filterUniqueNames = (cards) => {
     const uniqueNames = new Set();
     const uniqueCards = [];
 
-    for (const card of cards){
-      if (!uniqueNames.has(card.name)){
+    for (const card of cards) {
+      if (!uniqueNames.has(card.name)) {
         uniqueNames.add(card.name);
         uniqueCards.push(card);
       }
     }
     return uniqueCards;
-  }
-
-  const loadMoreCards = () => {
-    setCurrentPage(prevPage => prevPage + 1);
   };
 
-  return (
-    <div className='flex flex-col align-middle items-center'>
-      <h1 className='mx-auto font-Karantina text-orange-900 text-5xl flex mt-5 mb-5 uppercase font-bold'>Alexandria</h1>
-      <ul className='flex flex-wrap justify-center grid-cols-8 gap-6'>
-      {cards.map(card => (
-                    <li key={card.id} className='w-[250px] flex flex-col justify-center items-center'>
-                      <img src={card.imageUrl} alt="" className='rounded-[9px]' />
-                      <p className='font-bold text-orange-900'>{card.name}</p>
-                    </li>
-                ))}
-      </ul>
-      <button onClick={loadMoreCards} className='bg-orange-900 text-orange-200 px-6 py-3 mt-5 mb-5 rounded-md'>More Cards</button>
+  const loadMoreCards = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
 
-          </div>
-  )
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setSearch(query);
+  };
+
+  const queryCards =
+    search.trim() !== ''
+      ? cards.filter((card) =>
+          card.name.toLowerCase().includes(search.toLowerCase())
+        )
+      : cards;
+
+  const getRandomPlaceholder = async function fetchRandomNaame() {
+    try {
+      const response = await axios.get(
+        'https://api.magicthegathering.io/v1/cards?pageSize=100'
+      );
+      const randomIndex = Math.floor(
+        Math.random() * response.data.cards.length
+      );
+      const randomName = response.data.cards[randomIndex].name;
+      setRandomPlaceholder(randomName);
+    } catch (erro) {
+      console.log('Error: ', erro);
+    }
+  };
+
+  useEffect(() => {
+    getRandomPlaceholder();
+  }, []);
+
+  return (
+    <div className="flex flex-col align-middle items-center">
+      <h1 className="mx-auto font-Karantina text-orange-900 text-5xl flex mt-5 mb-5 uppercase font-bold">
+        Alexandria
+      </h1>
+      <nav>
+        <form action="">
+          <input
+            type="text"
+            onChange={handleSearch}
+            placeholder={randomPlaceholder}
+            className="my-3 w-[550px] h-8 rounded-sm indent-2 font-bold bg-orange-900 placeholder-orange-200 text-orange-200 placeholder-opacity-50 tracking-tight"
+          />
+        </form>
+      </nav>
+      <ul className="flex flex-wrap justify-center grid-cols-8 gap-6">
+        {queryCards.map((card) => (
+          <li
+            key={card.id}
+            className="w-[250px] flex flex-col justify-center items-center hover:mx-2 ease-in-out duration-150"
+          >
+            <img
+              src={card.imageUrl}
+              alt=""
+              className="rounded-[9px] hover:scale-125 ease-in-out duration-150 size-[350px]"
+            />
+            <p className="font-bold text-orange-900">{card.name}</p>
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={loadMoreCards}
+        className="bg-orange-900 text-orange-200 px-6 py-3 mt-5 mb-5 rounded-md"
+      >
+        More Cards
+      </button>
+    </div>
+  );
 }
