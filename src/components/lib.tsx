336ff415e175113d3436b625ef9d99a6ffd021d2
Levi Ribeiro <126264441+ribeiroLevi@users.noreply.github.com>
2024-04-09 18:19:40 +0000

    chore: update shadcn/ui dependencies

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,11 +1,25 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import axios from 'axios';
+import { useState, useEffect, ChangeEvent } from "react";
+import axios from "axios";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
+
+interface Card {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
 
 export function Lib() {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState<Card[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [search, setSearch] = useState('');
-  const [randomPlaceholder, setRandomPlaceholder] = useState('');
+  const [search, setSearch] = useState("");
+  const [randomPlaceholder, setRandomPlaceholder] = useState("");
   const pageSize = 100;
 
   useEffect(() => {
@@ -17,14 +31,14 @@ export function Lib() {
         const filteredCards = filterUniqueNames(response.data.cards);
         setCards((prevCards) => [...prevCards, ...filteredCards]);
       } catch (error) {
-        console.log('Error', error);
+        console.log("Error", error);
       }
     }
     fetchData();
   }, [currentPage]);
 
   // Filter out cards with duplicate names
-  const filterUniqueNames = (cards) => {
+  const filterUniqueNames = (cards: Card[]) => {
     const uniqueNames = new Set();
     const uniqueCards = [];
 
@@ -47,7 +61,7 @@ export function Lib() {
   };
 
   const queryCards =
-    search.trim() !== ''
+    search.trim() !== ""
       ? cards.filter((card) =>
           card.name.toLowerCase().includes(search.toLowerCase())
         )
@@ -56,7 +70,7 @@ export function Lib() {
   const getRandomPlaceholder = async function fetchRandomNaame() {
     try {
       const response = await axios.get(
-        'https://api.magicthegathering.io/v1/cards?pageSize=100'
+        "https://api.magicthegathering.io/v1/cards?pageSize=100"
       );
       const randomIndex = Math.floor(
         Math.random() * response.data.cards.length
@@ -64,7 +78,7 @@ export function Lib() {
       const randomName = response.data.cards[randomIndex].name;
       setRandomPlaceholder(randomName);
     } catch (erro) {
-      console.log('Error: ', erro);
+      console.log("Error: ", erro);
     }
   };
 
@@ -77,6 +91,7 @@ export function Lib() {
       <h1 className="mx-auto font-Karantina text-orange-900 text-5xl flex mt-5 mb-5 uppercase font-bold">
         Alexandria
       </h1>
+
       <nav>
         <form action="">
           <input
@@ -89,17 +104,29 @@ export function Lib() {
       </nav>
       <ul className="flex flex-wrap justify-center grid-cols-8 gap-6">
         {queryCards.map((card) => (
-          <li
-            key={card.id}
-            className="w-[250px] flex flex-col justify-center items-center hover:mx-2 ease-in-out duration-150"
-          >
-            <img
-              src={card.imageUrl}
-              alt=""
-              className="rounded-[9px] hover:scale-125 ease-in-out duration-150 size-[350px]"
-            />
-            <p className="font-bold text-orange-900">{card.name}</p>
-          </li>
+          <Dialog key={card.id}>
+            <DialogTrigger asChild>
+              <li className="w-[250px] flex flex-col justify-center items-center hover:mx-2 ease-in-out duration-150">
+                <img
+                  src={card.imageUrl}
+                  alt=""
+                  className="rounded-[9px] hover:scale-125 ease-in-out duration-150 size-[350px]"
+                />
+
+                <p className="font-bold text-orange-900">{card.name}</p>
+              </li>
+            </DialogTrigger>
+
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         ))}
       </ul>
       <button
