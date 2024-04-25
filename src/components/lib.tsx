46c2c2d5f51bd909b482b05246c69c77b2c9b376
Levi Ribeiro <leviribeiro72@gmail.com>
2024-04-25 18:10:03 -0300

    :sparkles: feat/ui: infinite scroll/dialog colors

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -10,13 +10,12 @@ import {
 } from '../components/ui/dialog';
 
 import { Switch } from '../components/ui/switch';
-import { string, z } from 'zod';
+import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -37,6 +36,8 @@ type Card = {
   manaCost: string;
   colors: string[];
 };
+
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export function Lib() {
   const [cards, setCards] = useState<Card[]>([]);
@@ -124,224 +125,240 @@ export function Lib() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      filterRed: true,
-      filterBlue: true,
-      filterGreen: true,
-      filterBlack: true,
-      filterWhite: true,
+      filterRed: false,
+      filterBlue: false,
+      filterGreen: false,
+      filterBlack: false,
+      filterWhite: false,
     },
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    // Initialize the filters array
     let newFilters = [];
 
-    // Add the selected colors to the filters array
     if (data.filterBlack) newFilters.push('B');
     if (data.filterBlue) newFilters.push('U');
     if (data.filterRed) newFilters.push('R');
     if (data.filterGreen) newFilters.push('G');
     if (data.filterWhite) newFilters.push('W');
 
-    // Construct the query string with the selected colors
     const queryString =
       newFilters.length > 0 ? `&colors=${newFilters.join('&')}` : '';
-
-    // Log the constructed query string
-    console.log(queryString);
-
-    // Set the filters state with the new query string
     setCards([]);
     setFilters(queryString);
   }
 
   return (
-    <div className="flex flex-col align-middle items-center">
-      <h1 className="mx-auto font-Karantina text-orange-900 text-5xl flex mt-5 mb-5 uppercase font-bold">
-        Alexandria
-      </h1>
-      <nav className="flex flex-row w-[800px] items-center justify-around">
-        <form action="">
-          <input
-            type="text"
-            onChange={handleSearch}
-            placeholder={randomPlaceholder}
-            className="my-3 w-[550px] h-8 rounded-sm indent-2 font-bold bg-orange-900 placeholder-orange-200 text-orange-200 placeholder-opacity-50 tracking-tight"
-          />
-        </form>
-        <Dialog>
-          <DialogTrigger>
-            <button className="bg-orange-900 w-[100px] h-8 font-bold rounded-sm text-orange-200">
-              Filtros
-            </button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>
-                <div className="w-full flex flex-col items-center">
-                  <p className="text-4xl text-orange-900">Filtros</p>{' '}
-                  <p className="text-xl text-orange-900">
-                    Selecione um ou mais filtos e pressione "Confirmar"!
-                  </p>
-                </div>
-              </DialogTitle>
-              <DialogDescription>
-                <Form {...form}>
-                  <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <div className="w-full flex flex-row items-center justify-around text-xl text-orange-900 mt-4">
-                      <FormField
-                        control={form.control}
-                        name="filterWhite"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>White</FormLabel>
-                            <FormControl>
-                              <Switch
-                                checked={field.value}
-                                onCheckedChange={field.onChange}
-                              />
-                            </FormControl>
-                          </FormItem>
-                        )}
-                      ></FormField>
-                      <FormField
-                        control={form.control}
-                        name="filterRed"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Red</FormLabel>
-                            <FormControl>
-                              <Switch
-                                checked={field.value}
-                                onCheckedChange={field.onChange}
-                              />
-                            </FormControl>
-                          </FormItem>
-                        )}
-                      ></FormField>
-                      <FormField
-                        control={form.control}
-                        name="filterBlue"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Blue</FormLabel>
-                            <FormControl>
-                              <Switch
-                                checked={field.value}
-                                onCheckedChange={field.onChange}
-                              />
-                            </FormControl>
-                          </FormItem>
-                        )}
-                      ></FormField>
-                      <FormField
-                        control={form.control}
-                        name="filterGreen"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Green</FormLabel>
-                            <FormControl>
-                              <Switch
-                                checked={field.value}
-                                onCheckedChange={field.onChange}
-                              />
-                            </FormControl>
-                          </FormItem>
-                        )}
-                      ></FormField>
-                      <FormField
-                        control={form.control}
-                        name="filterBlack"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Black</FormLabel>
-                            <FormControl>
-                              <Switch
-                                checked={field.value}
-                                onCheckedChange={field.onChange}
-                              />
-                            </FormControl>
-                          </FormItem>
-                        )}
-                      ></FormField>
-                      <Button type="submit">Submit</Button>
-                    </div>
-                  </form>
-                </Form>
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      </nav>
-      <ul className="flex flex-wrap justify-center grid-cols-8 gap-6 ">
-        {queryCards.map((card) => (
-          <Dialog key={card.id}>
-            <DialogTrigger asChild>
-              <li className="w-[250px] flex flex-col justify-center items-center hover:mx-1 ease-in-out duration-150 hover:cursor-pointer">
-                <img
-                  src={card.imageUrl}
-                  alt=""
-                  className="rounded-[9px] hover:scale-125 ease-in-out duration-150 size-[350px]"
-                />
-                <p className="font-bold text-orange-900">{card.name}</p>
-              </li>
+    <InfiniteScroll
+      dataLength={cards.length} // This is important field to render the next data
+      next={loadMoreCards} // Pass the function that will load more data
+      hasMore={true} // Change this to false if there are no more records to be fetched
+      loader={<h4 className="flex item-center">Loading...</h4>}
+      endMessage={
+        <p className="">
+          <b>You have seen it all</b>
+        </p>
+      }
+    >
+      <div className="flex flex-col align-middle items-center">
+        <h1 className="mx-auto font-Karantina text-orange-900 text-5xl flex mt-5 mb-5 uppercase font-bold">
+          Alexandria
+        </h1>
+        <nav className="flex flex-row w-[800px] items-center justify-around">
+          <form action="">
+            <input
+              type="text"
+              onChange={handleSearch}
+              placeholder={randomPlaceholder}
+              className="my-3 w-[550px] h-8 rounded-sm indent-2 font-bold bg-orange-900 placeholder-orange-200 text-orange-200 placeholder-opacity-50 tracking-tight"
+            />
+          </form>
+          <Dialog>
+            <DialogTrigger>
+              <button className="bg-orange-900 w-[100px] h-8 font-bold rounded-sm text-orange-200">
+                Filtros
+              </button>
             </DialogTrigger>
             <DialogContent>
               <DialogHeader>
-                <DialogTitle className="text-5xl">
-                  <div className="flex flex-col text-orange-900">
-                    {card.name} <span className="text-2xl">{card.type}</span>
+                <DialogTitle>
+                  <div className="w-full flex flex-col items-center">
+                    <p className="text-4xl text-orange-900">Filtros</p>{' '}
+                    <p className="text-xl text-orange-900">
+                      Selecione um ou mais filtos e pressione "Confirmar"!
+                    </p>
                   </div>
                 </DialogTitle>
-                <DialogDescription className="flex flex-row items-center">
-                  <div className="text-orange-700">
-                    <div className="flex flex-row justify-between w-[300px]">
-                      <p className="text-3xl mt-3">
-                        <span className="text-orange-900">Mana Value:</span>{' '}
-                        {card.cmc}
-                      </p>
-                      <p className="text-3xl mt-3">
-                        <span className="text-orange-900">Mana Cost:</span>{' '}
-                        {card.manaCost}
-                      </p>
-                    </div>
-                    <p className="text-3xl w-[300px] mt-3">
-                      <span className="text-orange-900">Texto da Carta:</span>{' '}
-                      <br />
-                      {card.originalText}
-                    </p>
-                    <div className="flex flex-row mt-3 w-[400px] justify-between">
-                      <p className="text-3xl">
-                        <span className="text-orange-900">P/T:</span>
-                        {card.power}/{card.toughness}
-                      </p>
-                      <p className="text-3xl">
-                        <span className="text-orange-900">Expansion:</span>{' '}
-                        {card.setName}
-                      </p>
-                      <p className="text-3xl">
-                        <span className="text-orange-900">Rarity:</span>{' '}
-                        {card.rarity}
-                      </p>
-                    </div>
-                  </div>
-                  <img
-                    className="rounded-lg size-[500px] mx-4"
-                    src={card.imageUrl}
-                    alt=""
-                  />
+                <DialogDescription>
+                  <Form {...form}>
+                    <form
+                      onSubmit={form.handleSubmit(onSubmit)}
+                      className="flex flex-col items-center gap-4"
+                    >
+                      <div className="w-full flex flex-row items-center justify-around text-xl text-orange-900 mt-4">
+                        <FormField
+                          control={form.control}
+                          name="filterWhite"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="text-3xl mx-3">
+                                White
+                              </FormLabel>
+                              <FormControl>
+                                <Switch
+                                  checked={field.value}
+                                  onCheckedChange={field.onChange}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          )}
+                        ></FormField>
+                        <FormField
+                          control={form.control}
+                          name="filterRed"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="text-3xl mx-3">
+                                Red
+                              </FormLabel>
+                              <FormControl>
+                                <Switch
+                                  checked={field.value}
+                                  onCheckedChange={field.onChange}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          )}
+                        ></FormField>
+                        <FormField
+                          control={form.control}
+                          name="filterBlue"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="text-3xl mx-3">
+                                Blue
+                              </FormLabel>
+                              <FormControl>
+                                <Switch
+                                  checked={field.value}
+                                  onCheckedChange={field.onChange}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          )}
+                        ></FormField>
+                        <FormField
+                          control={form.control}
+                          name="filterGreen"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="text-3xl mx-3">
+                                Green
+                              </FormLabel>
+                              <FormControl>
+                                <Switch
+                                  checked={field.value}
+                                  onCheckedChange={field.onChange}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          )}
+                        ></FormField>
+                        <FormField
+                          control={form.control}
+                          name="filterBlack"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="text-3xl mx-3">
+                                Black
+                              </FormLabel>
+                              <FormControl>
+                                <Switch
+                                  checked={field.value}
+                                  onCheckedChange={field.onChange}
+                                />
+                              </FormControl>
+                            </FormItem>
+                          )}
+                        ></FormField>
+                      </div>
+                      <Button
+                        type="submit"
+                        className="bg-orange-900 w-[200px] h-[40px] text-2xl text-orange-200"
+                      >
+                        Submit
+                      </Button>
+                    </form>
+                  </Form>
                 </DialogDescription>
               </DialogHeader>
             </DialogContent>
           </Dialog>
-        ))}
-      </ul>
-      <button
-        onClick={loadMoreCards}
-        className="bg-orange-900 text-orange-200 px-6 py-3 mt-5 mb-5 rounded-md"
-      >
-        More Cards
-      </button>
-    </div>
+        </nav>
+        <ul className="flex flex-wrap justify-center grid-cols-8 gap-6 ">
+          {queryCards.map((card) => (
+            <Dialog key={card.id}>
+              <DialogTrigger asChild>
+                <li className="w-[250px] flex flex-col justify-center items-center hover:mx-1 ease-in-out duration-150 hover:cursor-pointer">
+                  <img
+                    src={card.imageUrl}
+                    alt=""
+                    className="rounded-[9px] hover:scale-125 ease-in-out duration-150 size-[350px]"
+                  />
+                  <p className="font-bold text-orange-900">{card.name}</p>
+                </li>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle className="text-5xl">
+                    <div className="flex flex-col text-orange-900">
+                      {card.name} <span className="text-2xl">{card.type}</span>
+                    </div>
+                  </DialogTitle>
+                  <DialogDescription className="flex flex-row items-center">
+                    <div className="text-orange-700">
+                      <div className="flex flex-row justify-between w-[300px]">
+                        <p className="text-3xl mt-3">
+                          <span className="text-orange-900">Mana Value:</span>{' '}
+                          {card.cmc}
+                        </p>
+                        <p className="text-3xl mt-3">
+                          <span className="text-orange-900">Mana Cost:</span>{' '}
+                          {card.manaCost}
+                        </p>
+                      </div>
+                      <p className="text-3xl w-[300px] mt-3">
+                        <span className="text-orange-900">Texto da Carta:</span>{' '}
+                        <br />
+                        {card.originalText}
+                      </p>
+                      <div className="flex flex-row mt-3 w-[400px] justify-between">
+                        <p className="text-3xl">
+                          <span className="text-orange-900">P/T:</span>
+                          {card.power}/{card.toughness}
+                        </p>
+                        <p className="text-3xl">
+                          <span className="text-orange-900">Expansion:</span>{' '}
+                          {card.setName}
+                        </p>
+                        <p className="text-3xl">
+                          <span className="text-orange-900">Rarity:</span>{' '}
+                          {card.rarity}
+                        </p>
+                      </div>
+                    </div>
+                    <img
+                      className="rounded-lg size-[500px] mx-4"
+                      src={card.imageUrl}
+                      alt=""
+                    />
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          ))}
+        </ul>
+      </div>
+    </InfiniteScroll>
   );
 }
